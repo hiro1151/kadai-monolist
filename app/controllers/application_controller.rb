a3d96fc1ae90e6eaa34e2bce28e47cd5367332a5
hiro1151 <hiro.gitcut77@gmail.com>
2017-10-20 14:49:06 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,21 @@ class ApplicationController < ActionController::Base
 
   def require_user_logged_in
     unless logged_in?
-    redirect_to login_url
+      redirect_to login_url
+    end
   end
- end
+
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,39 +4,44 @@ class ItemsController < ApplicationController
   def new
     @items = []
   
-  
-  @keyword = params[:keyword]
-  if @keyword
-    results = RakutenWebService::Ichiba::Item.search({
-      keyword: @keyword,
-      imageFlag: 1,
-      hits: 20,
-    })
-    
-    results.each do |result|
-      # 扱い易いようにItemとしてインスタンスを作成する（保存はしない）
-      item = Item.new(read(result))
-      @items << item
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+      
+      results.each do |result|
+        # 扱い易いようにItemとしてインスタンスを作成する（保存はしない）
+        item = Item.find_or_initialize_by(read(result))
+        @items << item
+      end
     end
   end
+
+  def show
+  @item = Item.find(params[:id])
+  @want_users = @item.want_users
   end
+
 
 private
 
-def read(result)
-  code = result['itemCode']
-  name = result['itemName']
-  url = result['itemUrl']
-  image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128','')
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128','')
+    
+  return{
+    code: code,
+    name: name,
+    url: url,
+    image_url: image_url,
+    }
   
-return{
-  code: code,
-  name: name,
-  url: url,
-  image_url: image_url,
-  }
-
-end
+  end
   
 end
   
